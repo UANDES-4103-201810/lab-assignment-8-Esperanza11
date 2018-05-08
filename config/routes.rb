b8f4c46727ca9bf006c5c1281cec5f432148c942
jauger77 <lilatuz@miuandes.cl>
2018-05-08 13:16:34 -0300

    RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   resources :users
+  get '/users/:id/', to: 'users#show'
+  get '/users/', to: 'users#index'
+  post '/users/create/', to: 'users#create'
+  put '/users/:id/', to: 'users#update'
+  delete '/users/:id/', to: 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
